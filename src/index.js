74fa05e5321d87d2a15007a05dd4c73adaf77718
Chandhanasenthil <chandhanasenthil@gmail.com>
2026-07-03 14:34:49 +0530

    random number generator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
+import {useState} from 'react';
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Randomnumber(){
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var [count,setCount] = useState(0);
+function generaterandomnumber()
+{
+    setCount(Math.floor(Math.random() * 100)+1)
+}
+function reset(){
+    setCount(0)
+}
+return(
+    <div className="container">
+        <h1 className="count">Random Number Generator</h1>
+        {count===0?<p className="count">No Number Generated Yet</p>:<p className="count">{count}</p>}
+        <button className="btn" onClick={generaterandomnumber}>Generate Random Number</button>
+        <button className="btn"  onClick={reset}>Reset</button>
+        
+    </div>
+)}
+root.render(<Randomnumber />);
